8ff633367a47dfb68dc7062d669c24876330cea1
Bryan <bryanbuitrago86@gmail.com>
2017-12-13 19:19:16 +0000

    Unfinished Tut

---
+++ CTutorial1.c
@@ -0,0 +1,19 @@
+/* Multiline
+Comment */
+
+// Single line Comment
+
+#include <stdio.h>
+#include <string.h>
+
+#define MYNAME "Derek Banas"
+
+int globalVar = 100;
+
+main() {
+  char firstLetter = 'D';
+
+  int age = 38;
+
+  long int superBigNum
+}
